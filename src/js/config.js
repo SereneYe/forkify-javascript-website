5bda7ed28086dac6568ca6e40139d6f5c45338b7
sereneye <sereneye130@gmail.com>
2024-01-19 10:46:03 +1000

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1f89fad6-3447-460a-800e-f8e7494aea3c';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/view/View.js
@@ -0,0 +1,101 @@
+import icons from 'url:../../img/icons.svg';
+
+export default class View {
+  _data;
+
+  render(data, render = true) {
+    // if (!data || (Array.isArray(data) && data.length === 0))
+    //   return this.renderError();
+    this._data = data;
+    const markup = this._generateMarkup();
+    /* 
+    Why flag parameter ?
+     This is because when previewView trying to call render function to generate markup
+     This function will render something on the page
+     But we only want previewView to return strings(also bookmarks and results's _generateMarkup())
+
+     And then rendered by the controller of SPECIFIED strings by called bookmarks.render()
+     Why didn't use the previewView._generateMarkup()?
+     Cause in  _generateMarkup() we have direct access to the whole data by this._data, then previewView have the whole access of all the view data and then return all the data.
+     only the _generateMarkup called by render() can specified the passed in data
+     */
+    if (!render) return markup;
+
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  update(data) {
+    if (!data || (Array.isArray(data) && data.length === 0))
+      return this.renderError();
+    this._data = data;
+    const newMarkup = this._generateMarkup();
+
+    const newDOM = document.createRange().createContextualFragment(newMarkup);
+    const newElements = Array.from(newDOM.querySelectorAll('*'));
+    const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+      // console.log(curEl, newEl.isEqualNode(curEl));
+
+      // Updates changed TEXT
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ''
+      ) {
+        // console.log('💥', newEl.firstChild.nodeValue.trim());
+        curEl.textContent = newEl.textContent;
+      }
+
+      // Updates changed ATTRIBUTES
+      if (!newEl.isEqualNode(curEl))
+        Array.from(newEl.attributes).forEach(attr =>
+          curEl.setAttribute(attr.name, attr.value)
+        );
+    });
+  }
+
+  _clear() {
+    this._parentElement.innerHTML = '';
+  }
+
+  renderSpinner() {
+    const markup = `
+  <div class="spinner">
+          <svg>
+            <use href="${icons}icon-loader"></use>
+          </svg>
+        </div> `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderError(message = this._errorMessage) {
+    const markup = `<div class="error">
+            <div>
+              <svg>
+                <use href="${icons}#icon-alert-triangle"></use>
+              </svg>
+            </div>
+            <p>${message}</p>
+          </div>`;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderMessage(message = this._message) {
+    const markup = `
+      <div class="message">
+        <div>
+          <svg>
+            <use href="${icons}#icon-smile"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
+    `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+}
